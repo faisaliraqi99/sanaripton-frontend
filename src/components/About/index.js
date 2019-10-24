79c4c04a8904e601172cb7cddf7bf09eeaa9882b
Faisal Iraqi <faisal.iraqi99@gmail.com>
2019-10-24 16:49:38 +0600

    About page default-container

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
+import './About.css';
 import NavBar from '../NavBar/NavBar';
 
 class About extends Component {
   render() {
     return (
-      <div>
+      <div className="about">
         <NavBar />
-        About
+        <div className="default-container">
+          About
+        </div>
       </div>
     );
   }

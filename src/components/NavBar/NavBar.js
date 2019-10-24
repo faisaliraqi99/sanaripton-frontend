37af7774d9f348063131b1a9018661438740e874
pipisasa <emir0725mk@gmail.com>
2019-10-24 10:09:42 +0600

    NavBar v1

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,8 +5,17 @@ import './NavBar.css';
 class NavBar extends Component {
   render() {
     return (
-      <div className="navbar">
-        Halo
+      <div className="header">
+        <div className="navbar header-left">
+        <a className="nav" href="#">Logo</a>
+        <a className="nav" href="#">Бот</a>
+        <a className="nav" href="#">Статистика</a>
+        <a className="nav" href="#">О нас</a>
+        </div>
+
+        <div className="header-right">
+          <a className="login" href="#">login</a>
+        </div>
       </div>
     );
   }

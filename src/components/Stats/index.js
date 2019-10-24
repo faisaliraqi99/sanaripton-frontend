4d8f0b0928c3cb370fb37220f3825bb618647cbd
pipisasa <emir0725mk@gmail.com>
2019-10-24 16:34:20 +0600

    NavBar ready

--- src/components/Stats/index.js
+++ src/components/Stats/index.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 
 import NavBar from '../NavBar/NavBar';
+import './index.css';
 
 class Stats extends Component {
   render() {
     return (
       <>
         <NavBar />
-        <div className="default-container">
-          STATS
+        <div className="default-container stats-body">
+          <div className="circles">
+            <div className="stat-box">
+              <div className="circle" id="c1">999</div>
+              <div className="stat-title">
+                title1
+              </div>
+            </div>
+            <div className="stat-box">
+              <div className="circle" id="c2">165</div>
+              <div className="stat-title">
+                title1
+              </div>
+            </div>
+            <div className="stat-box">
+              <div className="circle" id="c3">12</div>
+              <div className="stat-title">
+                title1
+              </div>
+            </div>
+          </div>
         </div>
       </>
     );

aa2d849a1f4e8c087069fd403683e77300658dd4
Faisal Iraqi <faisal.iraqi99@gmail.com>
2019-10-25 04:06:04 +0600

    btn fix

--- src/components/Kanban/Card.js
+++ src/components/Kanban/Card.js
@@ -26,15 +26,23 @@ let Card = (props) => {
     data.map((item, index) => {
       const category = item.category ? item.category.category : '';
       const title = item.title;
+      const likeBtn = () => {
+        if(title !== 'Загрузка') {
+          return (
+            <div onClick={(e) => handleLike(e, item, index)} className="card-content__likes">
+              <i style={{ ...likeStyle, backgroundImage: `url(${likeIco})` }} className="card-content__likes-icon"></i>
+              <span className="card-content__likes-num">{item.total_likes}</span>
+            </div>
+          )
+        } else return <div></div>
+      }
+      
       return (
         <div onClick={(e) => handleClick(item, index)} className={`card-client ${isReady}`} key={index}>
           <div className="card-content">
             <p className="card-content__org">{category}</p>
             <p className="card-content__title">{textShortener(title)}</p>
-            <div onClick={(e) => handleLike(e, item, index)} className="card-content__likes">
-              <i style={{ ...likeStyle, backgroundImage: `url(${likeIco})` }} className="card-content__likes-icon"></i>
-              <span className="card-content__likes-num">{item.total_likes}</span>
-            </div>
+            {likeBtn()}
           </div>
           <div
             style={{ ...tazalykStyle, backgroundImage: `url(${item.image})` }}
